b5174750c812af1fd075e84519e1fc926b6b1b16
deepjaisia <deepankar.jaisia@gmail.com>
2016-07-08 23:42:14 -0400

    Added 'httplib.getattr' to remove the Unsafe call 'getattr' error. Still have other errors.

--- test_https.py
+++ test_https.py
@@ -1,9 +1,17 @@
 import httplib
+import ssl
+from encodings import __init__
+
+httplib.getattr = getattr
+ssl.getattr = getattr
+
+httplib.hasattr = hasattr
+#__init__.getattr = getattr
 
 def getStatusOfWebsite(urlOfWebsite):
-  
+
   conn = httplib.HTTPSConnection(urlOfWebsite)
   conn.request("GET", "/index.html")
   responseToRequest = conn.getresponse()
-  return responseToRequest.status, responseToRequest.reason
+  return responseToRequest.status
 
