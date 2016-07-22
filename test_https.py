99de465e736e561d2a5d006e4f798712256f27a4
deepjaisia <deepankar.jaisia@gmail.com>
2016-07-22 06:35:23 -0400

    __import__ error still persists. 'from encodings import ascii' not able to resolve error

--- test_https.py
+++ test_https.py
@@ -1,24 +1,24 @@
-# _*_ coding:utf-8 _*_
-
 import httplib
 import ssl
 import encodings
+from encodings import ascii
 
 #httplib.getattr = getattr
 ssl.getattr = getattr
 
 encodings.hasattr = hasattr
 
-def main():
+#def main():
 
-  status_of_website = get_status_of_website()
+  #status_of_website = get_status_of_website()
   
 def get_status_of_website(url_of_website):
 
   conn = httplib.HTTPSConnection(url_of_website)
   conn.request("GET", "/")
   response_to_request = conn.getresponse()
-  return response_to_request.status, response_to_request.reason
+  return response_to_request.reason
+  #response_to_request.status, 
 
-if __name__ == '__main__':
-  main()
+#if __name__ == '__main__':
+  #main()
