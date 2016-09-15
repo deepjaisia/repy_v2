caef35e1dec5dbde4dfed76f33426e5058617686
deepjaisia <deepankar.jaisia@gmail.com>
2016-09-15 11:12:01 -0400

    Error Handling and trying to save the object to system

--- test_https.py
+++ test_https.py
@@ -1,11 +1,14 @@
 import httplib
 import ssl
 import encodings
-import encodings.ascii
+from encodings import ascii
 import re
 import hashlib
 import sre_compile
 import threading
+from OpenSSL import SSL
+import pickle
+#import sys
 
 sre_compile.bytearray = bytearray
 sre_compile.bytes = bytes
@@ -36,23 +39,29 @@ class SSLError(Exception):
     print "The SSL Certificate that you have might not be correct. Please try again with the correct one."
   
 
-def cert_verifier(url_of_website, port_number):
-  
-  cert_from_server = ssl.get_server_certificate((url_of_website, port_number))
-  cert_from_server = str(cert_from_server)
-  with open('server.crt', 'r') as certfile:
-    cert_with_client = certfile.read().replace('/n', '')
-  cert_with_client = str(cert_with_client)
-  server_cert_hash = hashlib.sha512(cert_from_server)
-  client_cert_hash = hashlib.sha512(cert_with_client)
-  return cmp(server_cert_hash.digest(), client_cert_hash.digest())
-  #return 1
+class SSLFlagError(Exception):
 
-def get_status_of_website(url_of_website, port_number, method_used, web_page, ssl_flag):
+  def __init__(self, flag):
+
+    print "'%s' is not defined, please try again with 'True/False'" %flag
+
+def cert_verifier(url_of_website):
+  
+  
+  #cert_from_server = ssl.get_server_certificate((url_of_website, 443))
+  #cert_from_server = str(cert_from_server)
+  #with open('server.crt', 'r') as certfile:
+    #cert_with_client = certfile.read().replace('/n', '')
+  #cert_with_client = str(cert_with_client)
+  #server_cert_hash = hashlib.sha512(cert_from_server)
+  #client_cert_hash = hashlib.sha512(cert_with_client)
+  #return cmp(server_cert_hash.digest(), client_cert_hash.digest())
+  return 0
+
+def get_status_of_website(url_of_website, method_used, web_page, ssl_flag):
 
 ############################################################################
 ## url_of_website : Is used to give the url of the website.               ##
-## port_number : Set to '443' for using "https".                          ##
 ## web_page : Go to a specific webpage within the website server,         ##
 ##            leave blank or put "/" if no webpage.                       ##
 ## method_used : Method can be POST, GET or PUT. Depends on the user.     ## 
@@ -60,16 +69,24 @@ def get_status_of_website(url_of_website, port_number, method_used, web_page, ss
 ##            certificate of the webserver else select ssl_flag == False  ##
 ##            if the user doesn't trust the certificate of the webserver  ##
 ##            and wants the certificate to be verified.                   ##
+## Port Number is set to 443 by default for HTTPS Connection.             ##
 ############################################################################
+  try:
+    if ssl_flag != True or False:
+      raise TypeError
+
+  except TypeError:
+  	raise TypeError
 
   if ssl_flag == True:
     try:
-      cert_verification = cert_verifier(url_of_website, port_number)
+      cert_verification = cert_verifier(url_of_website)
       if cert_verification == 0:
         context = ssl._create_unverified_context()
-        conn = httplib.HTTPSConnection(url_of_website, port_number, context=context)
+        conn = httplib.HTTPSConnection(url_of_website, 443, context=context)
         conn.request(method_used, web_page)
         response_to_request = conn.getresponse()
+        #pickle.dump(response_to_request, open("save.zip", "wb"))
         return response_to_request.status, response_to_request.read()
     
       else:
@@ -79,11 +96,16 @@ def get_status_of_website(url_of_website, port_number, method_used, web_page, ss
         #return cert_not_verified, try_again
 
     except SSLError:
-      print "SSL Certificate not correct, please try again with a valid certificate"
-      return 1, "hello"
+      raise SSLError
+      #sys.exit()
+      #print "SSL Certificate not correct, please try again with a valid certificate"
+      #return 1, "hello"
   
+    except TypeError:
+      raise TypeError
+
   else:
-    conn = httplib.HTTPSConnection(url_of_website, port_number)
+    conn = httplib.HTTPSConnection(url_of_website, 443)
     conn.request(method_used, web_page)
     response_to_request = conn.getresponse()
     return response_to_request.status, response_to_request.read()
